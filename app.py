b4b3396f49a905511e1b8c97c653cdaf8ff128f9
Flukus99 <elementary@outlook.es>
2023-03-18 21:15:21 -0500

    solucion cors

--- app.py
+++ app.py
@@ -3,20 +3,20 @@ from ia_unica import ia_funcional
 from ia_multi import llamar
 import json
 from os import getcwd,remove,path
-from flask_cors import cross_origin
+from flask_cors import CORS
 
 
 PATH_FILE=getcwd()+"/files/"
 PATH_FILE2=getcwd()
 app= Flask(__name__)
 
-
+CORS(app)
 
 @app.route('/')
 def index():
     return 'el servidor esta funcionando'
 
-@cross_origin
+
 @app.route('/unico',methods=["POST"])
 def hello():
     peso=request.json['peso']
@@ -28,7 +28,7 @@ def hello():
     except:
         return jsonify({"error":"algo salio mal"})    
     
-@cross_origin
+
 @app.route('/multi/<string:token>', methods=["POST"])
 def hacer_multi(token):
     archivo=request.files["file"]
@@ -38,7 +38,7 @@ def hacer_multi(token):
     llamar(url,token)
     return send_from_directory(PATH_FILE2,token+".json",as_attachment=True)
  
-@cross_origin
+
 @app.route('/multi_delete/<string:token>', methods=["DELETE"])
 def delete_data(token):
     if path.isfile(PATH_FILE + token +".xlsx")==False:
